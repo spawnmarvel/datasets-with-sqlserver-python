759a84c628799887f166fe6b21c65f5f264c1060
spawnmarvel <espenkleivane@gmail.com>
2021-07-08 18:50:09 +0200

    simulated keyvault with json, read and .gitignore

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 
-import utility_db.db_connector as db_con
+from utility_db.db_connector import DbConnector
 import utility_reader.read_datasets as read_data
 from utility_logs.app_logger import Logger
+import json
 
 logger = Logger().get()
+
+def read_keyvault():
+    logger.info("Try retrieve password ")
+    try:
+        with open("keyvault.json") as fi:
+            data = json.load(fi)
+            p = data["password"]
+            print(p)
+            logger.info("Succsess retrieved password")
+            return p
+    except Exception as e:
+        logger.error(e)
+        logger.error("Failed to retrieved password")
+
 def main():
     logger.info("Main")
-    # db_con.get_version()
+    db = "DataSetsDb" 
+    user = "misp"
+    p = read_keyvault()
+    db_con = DbConnector(database=db, db_username=user, db_password=p)
     db_con.get_all_bestsellers()
-    read_data.read_file()
+    # read_data.read_file()
 
 if __name__ == "__main__":
     main()
--- utility_db/db_connector.py
+++ utility_db/db_connector.py
@@ -7,36 +7,32 @@ from utility_logs.app_logger import Logger
 
 logger = Logger().get()
 
+class DbConnector:
+
+    def __init__(self, database, db_username, db_password):
+        self.server = r"localhost\sqlexpress"
+        self.database = database
+        self.username = db_username
+        self.password = db_password
+        self.cnxn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER="+self.server+";DATABASE="+self.database+";UID="+self.username+";PWD="+ self.password)
+        self.cursor = self.cnxn.cursor()
+
+    def get_version(self):
+        self.cursor.execute("SELECT @@version;") 
+        row = self.cursor.fetchone() 
+        while row: 
+            print(row[0])
+            row = self.cursor.fetchone()
 
 
-server = r"localhost\sqlexpress"
-database = "DataSetsDb" 
-username = "misp" 
-password = "astring6r473" # this will never be done in production.....
-cnxn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER="+server+";DATABASE="+database+";UID="+username+";PWD="+ password)
-cursor = cnxn.cursor()
+    def get_all_bestsellers(self):
+        sql = "SELECT * FROM test.BestSellers;"
+        logger.info(sql)
+        self.cursor.execute(sql) 
+        row = self.cursor.fetchall() 
+        print(row)
 
-
-def get_version():
-    cursor.execute("SELECT @@version;") 
-    row = cursor.fetchone() 
-    while row: 
-        print(row[0])
-        row = cursor.fetchone()
-
-
-def get_all_bestsellers():
-    sql = "SELECT * FROM test.BestSellers;"
-    logger.info(sql)
-    cursor.execute(sql) 
-    row = cursor.fetchall() 
-    print(row)
-
-def insert_bestsellers(b_name, b_author, b_rating, b_price, b_year, b_genre):
-    print(b_name)
+    def insert_bestsellers(b_name, b_author, b_rating, b_price, b_year, b_genre):
+        print(b_name)
     # cursor.execute("""INSERT INTO test.BestSellers (b_name, b_author, b_rating, b_price, b_year, b_genre) VALUES (?,?,?,?,?,?)""", "P3","EAR") 
     # cnxn.commit()
-
-
-
-
