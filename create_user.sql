9003776663793bff869005af3c291e194dd9f79a
spawnmarvel <espenkleivane@gmail.com>
2021-07-08 19:17:01 +0200

    removed sensitive info 2

--- create_user.sql
+++ create_user.sql
@@ -1,6 +1,6 @@
 -- Creates the login misp with password provided in ''
 -- CREATE LOGIN misp WITH PASSWORD = '';  
-GO
+-- GO
 
 -- Creates a database user for the login created above.  
 CREATE USER misp FOR LOGIN misp;  
